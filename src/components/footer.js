5a3cab01024d914d9d0ad0a4c4212be60b8c5c46
shawn-ycx <shawnycx@gmail.com>
2018-08-18 20:14:44 -0500

    Lareinart project first commit

---
+++ src/components/footer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { push } from 'gatsby';
+
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+
+const styles = theme => ({
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        marginTop: theme.spacing.unit * 8,
+        padding: `${theme.spacing.unit * 6}px 0`,
+    },
+})
+
+const Footer = (props) => {
+    const { classes } = props;
+
+    return (
+        <footer className={classes.footer}>
+            <Typography variant="title" align="center">
+                Join my mailing list!
+            </Typography>
+            <Typography variant="subheading" align="center" color="textSecondary" gutterBottom>
+                Be the first to get notified with my lastest work!
+            </Typography>
+            <Typography align="center" color="textSecondary" component="p">
+                <Button 
+                    onClick={ () => push('/page-2/')}
+                    variant="outlined" size="small">Subscribe</Button>
+            </Typography>
+        </footer>
+    )
+}
+
+
+Footer.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Footer);
