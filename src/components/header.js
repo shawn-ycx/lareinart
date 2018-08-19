d5b2a880859141f22930743d98f9588cbbbc4b2f
shawn-ycx <shawnycx@gmail.com>
2018-08-19 03:17:12 -0500

    fixed production build

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Hidden from '@material-ui/core/Hidden';
 import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
 
 const styles = theme => ({
   layout: {
