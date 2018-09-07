6d4bd768fc3e017b973ddf1496ea5aac72b9223d
shawn-ycx <shawnycx@gmail.com>
2018-09-06 23:41:56 -0500

    Minor UI updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,15 +3,24 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
 const Contact = () => (
-  <div className="about-container">
-    <Link to="/">Back to Home</Link>
-    <article>
-      <h1>About Me</h1>
-      <h2>Lareina Yap</h2>
-      <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-1/38909266_10156385545041855_3706120790022815744_n.jpg?_nc_cat=0&oh=b7f9f1c7cbdb2cf1b1d86285df2e816c&oe=5C25F26B" />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sapiente laboriosam nostrum ratione? Quaerat, quidem magnam nostrum autem explicabo, deleniti debitis numquam, sapiente rem porro dolorum optio sit ex molestiae architecto! Pariatur ex ullam quos, expedita quo voluptatem esse a molestias sint maiores exercitationem tempora quam debitis, quas illum ab odio veniam eum iusto soluta vel nulla ea! Ut dicta distinctio laboriosam impedit fuga, placeat ipsum at. Tempore accusamus culpa similique eveniet ex in maxime quibusdam nam error exercitationem, nobis veniam fugiat voluptatum eligendi adipisci magnam rem labore aut. Aperiam quia a officia fuga molestias, ex ullam? Eligendi, perferendis quia!</p>
-    </article>
-  </div>
+  <Layout>
+    <div className="about-container">
+      {/* <article>
+        <h1>About Me</h1>
+        <h2>Lareina Yap</h2>
+        <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-1/38909266_10156385545041855_3706120790022815744_n.jpg?_nc_cat=0&oh=b7f9f1c7cbdb2cf1b1d86285df2e816c&oe=5C25F26B" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sapiente laboriosam nostrum ratione? Quaerat, quidem magnam nostrum autem explicabo, deleniti debitis numquam, sapiente rem porro dolorum optio sit ex molestiae architecto! Pariatur ex ullam quos, expedita quo voluptatem esse a molestias sint maiores exercitationem tempora quam debitis, quas illum ab odio veniam eum iusto soluta vel nulla ea! Ut dicta distinctio laboriosam impedit fuga, placeat ipsum at. Tempore accusamus culpa similique eveniet ex in maxime quibusdam nam error exercitationem, nobis veniam fugiat voluptatum eligendi adipisci magnam rem labore aut. Aperiam quia a officia fuga molestias, ex ullam? Eligendi, perferendis quia!</p>
+      </article> */}
+      <article className="profile-img">
+        <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-1/38909266_10156385545041855_3706120790022815744_n.jpg?_nc_cat=0&oh=b7f9f1c7cbdb2cf1b1d86285df2e816c&oe=5C25F26B" />
+      </article>
+      <article className="profile-text">
+        <h1>Lareina Yap</h1>
+        <h2>About Me</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nulla quas consectetur suscipit nam omnis dolorum maiores assumenda, quam corrupti minus hic aut neque ullam natus error! Impedit vel neque omnis, non cum dicta dolorem, maxime nostrum perferendis quisquam aliquid est labore culpa, magni ullam eius fugit sed. Excepturi animi quas temporibus. Cumque cum quasi, minus, inventore assumenda commodi repellat et nihil est quia totam dolorum velit. Quia distinctio nihil odio tenetur, accusamus corporis maiores eveniet iure, qui laboriosam esse! Temporibus ex dolore, animi tenetur nihil architecto molestias voluptates sunt, perspiciatis, ipsum perferendis aspernatur? Libero veritatis perspiciatis molestiae porro eum.</p>
+      </article>
+    </div>
+  </Layout>
 )
 
 export default Contact
